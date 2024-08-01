ac03ce0e9481e46a4526cc7c33530be900fcb40f
estebanLoza <estebanm.hloza@gmail.com>
2024-08-01 17:28:03 -0600

    Se avanza la sección de inicio, además ya se creo el componente de Navbar

---
+++ src/Components/Navbar.svelte
@@ -0,0 +1,76 @@
+<script>
+    //Importacion del logo de la empresa
+    import PLADAU from '../img/PLADAU.png';
+  
+      //variable para la navegacion y import de los componentes
+    export let menu = 1;
+    import Inicio from "../lib/Inicio.svelte";
+    import Nosotros from "../lib/Nosotros.svelte";
+</script>
+
+<header>
+  <nav>
+    <div>
+      <img src={PLADAU} alt="Logo de la empresa PLADAU">
+    </div>
+    <ul id="menu">
+      <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Inicio</a></li>
+      <li><a href="/" on:click|preventDefault={() => (menu = 2)} >Nosotros</a></li>
+      <li><a href="/" on:click|preventDefault={() => (menu = 3)} >Facultades</a></li>
+      <li><a href="/" on:click|preventDefault={() => (menu = 4)} >Inicio Sesión</a></li>
+      <li><a href="/" on:click|preventDefault={() => (menu = 5)} >Resgistrarse</a></li>
+    </ul>
+  </nav>
+</header>
+{#if menu === 1}
+  <Inicio />
+{:else if menu === 2}
+  <Nosotros />
+{/if}
+
+
+<style>
+  header{
+    width: 100%;
+    height: 5.625rem;
+    background-color:#6E98D8;
+  }
+  
+  nav{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  /*CAJA DEL LOGO*/
+  nav div{
+    border:1px solid red;
+    margin-left: 2%;
+  }
+  
+  
+  nav ul {
+    border: 0.5px solid green;
+    display: flex;
+    justify-content: flex-end;
+    gap:5%;
+    margin-right: 3%;
+  }
+  
+  nav ul li{
+    list-style: none;
+  }
+  
+  nav ul a{
+    font-family: kodchasan;
+    text-decoration:none;
+    color: white;
+    padding: 15%;
+  }
+  nav ul a:hover{
+    background-color: green;
+    border-radius: 15%;
+  }
+</style>
---
+++ src/lib/Inicio.svelte
@@ -0,0 +1,63 @@
+<script>
+  //importacion de imagenes
+  
+  import Bienvenida from "../img/Rectangle 3.png";
+</script>
+
+
+<div class="Bienvenida"
+      style="background-image:url({Bienvenida});
+              width: 100%;
+              height: 80vh;
+              background-position: center center;
+              background-repeat: no-repeat;
+              background-size: cover;
+            
+    ">
+  
+      <!--Contenedor de bienvenida-->
+      <div class="BienvenidoAutodidacta" style="
+                  background-color:rgba(11, 11, 11, 0.65);
+                  width: 60vw;
+                  height: 20vh;
+                  border-radius: 1rem;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: center;
+                  align-items: center;
+                  position: relative;
+                  left: 20%;
+                  top: 25%;
+                  ">
+        <h1>BIENVENIDO AUTODIDACTA</h1>
+        <h6>TE AYUDAREMOS A FORMARTE EN LO PROFESIONAL</h6>
+      </div>
+
+</div>
+
+
+<main>
+  <section>
+    <div>
+      
+    </div>
+    
+  </section>
+</main>
+
+
+
+
+<style>
+.BienvenidoAutodidacta h1, .BienvenidoAutodidacta h6{
+  color: white;
+  font-family: kodchasan;
+  
+}
+
+
+section{
+  height: 50vh;
+  width: 100%;
+}
+</style>
